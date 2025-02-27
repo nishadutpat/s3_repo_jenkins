c3636f822fc1f9970ed98ca196744737b8d24300
nishad <nishad@gmail.com>
2025-02-27 15:52:31 +0530

    crrated an iam role s3 bucket

--- main.tf
+++ main.tf
@@ -1,10 +1,49 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
+# IAM Role
+resource "aws_iam_role" "s3_admin_role" {
+  name = "S3AdminRole"
 
-resource "aws_s3_bucket" "test" {
-  bucket = "jenkins-s3_buclet"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"  # Change this if used for Lambda, ECS, etc.
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
 
+# Attach Administrator Policy to the Role
+resource "aws_iam_policy_attachment" "admin_policy" {
+  name       = "s3_admin_policy_attachment"
+  roles      = [aws_iam_role.s3_admin_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
 
+# (Optional) Create an S3 Bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "jenkins_bucket"
+}
+
+# (Optional) Attach IAM Role to the S3 Bucket Policy
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { AWS = aws_iam_role.s3_admin_role.arn }
+      Action    = "s3:*"
+      Resource  = [
+        "arn:aws:s3:::my-example-s3-bucket",
+        "arn:aws:s3:::my-example-s3-bucket/*"
+      ]
+    }]
+  })
 }
